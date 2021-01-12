02f18479a9f9dc7a363aefe88f6a9af1e5f2b46d
Asad Baidar <asadbaidar@gmail.com>
2021-01-12 05:12:12 +0500

    improvements

--- lib/src/res/themes.dart
+++ lib/src/res/themes.dart
@@ -5,30 +5,14 @@ import 'package:get_smart/src/utils/utils.dart';
 
 /// All app themes to be used in the app should be defined here
 class AppTheme {
-  static final kBackgroundLight = Colors.white;
-  static final kCanvasColorLight = Color(0xFFF2F2F2);
-  static final kPrimaryBackgroundLight = Colors.grey.shade50;
-  static final kBackgroundDark = Color(0xFF172329);
-  static final kCanvasColorDark = Color(0xFF26343C);
-  static final kPrimaryBackgroundDark = Colors.blueGrey.shade800;
-  static final kAccentColor = kPrimarySwatch.shade500;
-  static final kPrimarySwatch = Colors.blue;
-
-  static ThemeData light(BuildContext context) => _builder(
-        context,
-      );
-
-  static ThemeData dark(BuildContext context) => _builder(
-        context,
-        brightness: Brightness.dark,
-      );
-
-  static red(BuildContext context) => ThemeData.from(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.red,
-          brightness: brightness(context),
-        ),
-      );
+  static const kAccentColor = Color(0xFF2196F3);
+  static const kPrimarySwatch = Colors.blue;
+  static const kBackgroundLight = Colors.white;
+  static const kCanvasColorLight = Color(0xFFF2F2F2);
+  static const kPrimaryBackgroundLight = Color(0xFFFAFAFA);
+  static const kBackgroundDark = Color(0xFF172329);
+  static const kCanvasColorDark = Color(0xFF26343C);
+  static const kPrimaryBackgroundDark = Color(0xFF37474F);
 
   static bool isDark(BuildContext context) =>
       ThemeMode.system == ThemeMode.dark ||
@@ -45,23 +29,61 @@ class AppTheme {
   static Brightness getBrightness(bool isDark) =>
       isDark ? Brightness.dark : Brightness.light;
 
-  static ThemeData _builder(
+  static ThemeData red(BuildContext context) => ThemeData.from(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.red,
+          brightness: brightness(context),
+        ),
+      );
+
+  static ThemeData black(
     BuildContext context, {
-    Brightness brightness = Brightness.light,
+    Brightness brightness,
+  }) =>
+      builder(
+        context,
+        brightness: brightness,
+        accentColorLight: Colors.black,
+        accentColorDark: Colors.white,
+        primarySwatchLight: Colors.black,
+        primarySwatchDark: Colors.white,
+      );
+
+  static ThemeData sky(
+    BuildContext context, {
+    Brightness brightness,
+  }) =>
+      builder(context, brightness: brightness);
+
+  static ThemeData builder(
+    BuildContext context, {
+    Brightness brightness,
+    Color accentColorLight = kAccentColor,
+    Color primarySwatchLight = kPrimarySwatch,
+    Color backgroundLight = kBackgroundLight,
+    Color canvasColorLight = kCanvasColorLight,
+    Color primaryBackgroundLight = kPrimaryBackgroundLight,
+    Color accentColorDark = kAccentColor,
+    Color primarySwatchDark = kPrimarySwatch,
+    Color backgroundDark = kBackgroundDark,
+    Color canvasColorDark = kCanvasColorDark,
+    Color primaryBackgroundDark = kPrimaryBackgroundDark,
   }) {
-    final isDark = brightness == Brightness.dark;
-    final theme = ThemeData(brightness: brightness);
+    final _brightness = brightness ?? AppTheme.brightness(context);
+    final isDark = _brightness == Brightness.dark;
+    final theme = ThemeData(brightness: _brightness);
+    final _accentColor = isDark ? accentColorDark : accentColorLight;
     return ThemeData(
-      brightness: brightness,
-      backgroundColor: isDark ? kBackgroundDark : kBackgroundLight,
-      canvasColor: isDark ? kCanvasColorDark : kCanvasColorLight,
-      primarySwatch: kPrimarySwatch,
-      accentColor: kAccentColor,
+      brightness: _brightness,
+      backgroundColor: isDark ? backgroundDark : backgroundLight,
+      canvasColor: isDark ? canvasColorDark : canvasColorLight,
+      primarySwatch: primarySwatchLight,
+      accentColor: _accentColor,
       hintColor: theme.hintColor.hinted,
-      primaryColorBrightness: brightness,
-      primaryIconTheme: IconThemeData(color: kAccentColor),
-      iconTheme: IconThemeData(color: kAccentColor),
-      buttonColor: kAccentColor,
+      primaryColorBrightness: _brightness,
+      primaryIconTheme: IconThemeData(color: _accentColor),
+      iconTheme: IconThemeData(color: _accentColor),
+      buttonColor: _accentColor,
       buttonTheme: ButtonThemeData(
         textTheme: ButtonTextTheme.primary,
         shape: RoundedRectangleBorder(
@@ -70,30 +92,29 @@ class AppTheme {
       ),
       appBarTheme: AppBarTheme(
         elevation: 1,
-        color: isDark ? kPrimaryBackgroundDark : kPrimaryBackgroundLight,
+        color: isDark ? primaryBackgroundDark : primaryBackgroundLight,
       ),
       bottomAppBarTheme: BottomAppBarTheme(
         elevation: 4,
-        color: isDark ? kPrimaryBackgroundDark : kPrimaryBackgroundLight,
+        color: isDark ? primaryBackgroundDark : primaryBackgroundLight,
         shape: CircularNotchedRectangle(),
       ),
     );
   }
 
   /// Resolves which theme to use based on brightness.
-  static Widget builder(BuildContext context, Widget child) => Theme(
-        data: isDark(context) ? dark(context) : light(context),
+  static Widget defaultBuilder(BuildContext context, Widget child) => Theme(
+        data: sky(context),
         child: child,
       );
 
   static resetSystemChrome(BuildContext context) {
-    // var theme = Theme.of(context);
     var brightness = AppTheme.brightnessInverse(context);
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       // systemNavigationBarIconBrightness: brightness,
       statusBarBrightness: brightness,
       statusBarIconBrightness: brightness,
-      // systemNavigationBarColor: theme.bottomAppBarTheme.color,
+      // systemNavigationBarColor: Get.theme.bottomAppBarTheme.color,
       statusBarColor: Colors.transparent,
     ));
   }
