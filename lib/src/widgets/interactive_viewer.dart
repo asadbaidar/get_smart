c9f099c395eea92b114e341977999f67b66d116b
Asad Baidar <asadbaidar@gmail.com>
2021-07-28 12:42:27 +0500

    bug fixes

--- lib/src/widgets/interactive_viewer.dart
+++ lib/src/widgets/interactive_viewer.dart
@@ -5,30 +5,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get_smart/get_smart.dart';
 
-class InteractiveViewerOverlay extends StatefulWidget {
-  final Widget child;
-  final double maxScale;
-
-  const InteractiveViewerOverlay({
+class GetInteractiveViewer extends StatefulWidget {
+  const GetInteractiveViewer({
     required this.child,
+    this.minScale = 1.0,
     this.maxScale = 2.5,
+    this.barrierColor = Colors.black,
+    this.backgroundColor,
     Key? key,
   }) : super(key: key);
 
+  final Widget child;
+  final double minScale;
+  final double maxScale;
+  final Color barrierColor;
+  final Color? backgroundColor;
+
   @override
-  _InteractiveViewerOverlayState createState() =>
-      _InteractiveViewerOverlayState();
+  _GetInteractiveViewerState createState() => _GetInteractiveViewerState();
 }
 
-class _InteractiveViewerOverlayState extends State<InteractiveViewerOverlay>
+class _GetInteractiveViewerState extends State<GetInteractiveViewer>
     with SingleTickerProviderStateMixin {
   final viewerKey = GlobalKey();
+  final controller = TransformationController();
+  final scaleDetails = ScaleUpdateDetails().obs;
+  late AnimationController snap;
+
   Rect? placeholder;
   OverlayEntry? entry;
-  final controller = TransformationController();
   Matrix4Tween? snapTween;
-  final scale = ScaleUpdateDetails().obs;
-  late AnimationController snap;
 
   @override
   void initState() {
@@ -40,9 +46,7 @@ class _InteractiveViewerOverlayState extends State<InteractiveViewerOverlay>
       if (snap.isCompleted) {
         entry?.remove();
         entry = null;
-        setState(() {
-          placeholder = null;
-        });
+        setState(() => placeholder = null);
       }
     });
   }
@@ -53,92 +57,90 @@ class _InteractiveViewerOverlayState extends State<InteractiveViewerOverlay>
     super.dispose();
   }
 
-  Widget buildViewer(BuildContext context) {
-    return InteractiveViewer(
-        key: viewerKey,
-        transformationController: controller,
-        panEnabled: false,
-        maxScale: widget.maxScale,
-        child: widget.child,
-        onInteractionUpdate: (details) => scale.value = details,
-        onInteractionStart: (details) {
-          if (placeholder != null) return;
+  Widget buildViewer(BuildContext context) => InteractiveViewer(
+      key: viewerKey,
+      transformationController: controller,
+      panEnabled: false,
+      maxScale: 1,
+      child: widget.child,
+      onInteractionUpdate: (details) => scaleDetails.value = details,
+      onInteractionStart: (details) {
+        if (placeholder != null || details.pointerCount <= 1) return;
 
-          setState(() {
-            final renderViewer =
-                viewerKey.context!.findRenderObject() as RenderBox;
-            // final renderObject = Get.context!.findRenderObject() as RenderBox;
-            // placeholder = Rect.fromPoints(
-            //   renderViewer.localToGlobal(Offset.zero),
-            //   renderViewer
-            //       .localToGlobal(renderViewer.size.bottomRight(Offset.zero)),
-            // );
-            placeholder = Rect.fromCenter(
-              center: renderViewer
-                  .localToGlobal(renderViewer.size.center(Offset.zero)),
-              width: renderViewer.size.width,
-              height: renderViewer.size.height,
-            );
-          });
-
-          entry = OverlayEntry(
-            builder: (context) => Obx(
-              () {
-                final _scale = scale.value;
-                var isPanning = _scale.pointerCount > 1;
-                var point = isPanning ? _scale.focalPoint : placeholder!.center;
-                final scalar = max(1.0, min(_scale.scale, widget.maxScale));
-                print("scale ${_scale.scale} scalar $scalar");
-                return Container(
-                  color: Colors.black
-                      .withOpacity(max(1, min(_scale.scale, 1.70)) % 1),
-                  child: Stack(
-                    children: [
-                      AnimatedPositioned.fromRect(
-                        duration: (isPanning ? 50 : 150).milliseconds,
-                        curve: isPanning ? Curves.linear : Curves.easeOut,
-                        rect: Rect.fromCenter(
-                          center: point,
-                          width: placeholder!.width * scalar,
-                          height: placeholder!.height * scalar,
-                        ),
-                        child: buildViewer(context),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          );
-          Overlay.of(context)?.insert(entry!);
-        },
-        onInteractionEnd: (details) {
-          scale.value = ScaleUpdateDetails(
-            focalPoint: placeholder?.center ?? scale.value.focalPoint,
-            horizontalScale: scale.value.horizontalScale,
-            localFocalPoint: scale.value.localFocalPoint,
-            pointerCount: scale.value.pointerCount,
-            rotation: scale.value.rotation,
-            scale: scale.value.scale,
-            verticalScale: scale.value.verticalScale,
-          );
-          snapTween = Matrix4Tween(
-            begin: controller.value,
-            end: Matrix4.identity(),
-          );
-          snap.value = 0;
-          snap.animateTo(
-            1,
-            duration: Duration(milliseconds: 250),
-            curve: Curves.ease,
+        setState(() {
+          final renderViewer =
+              viewerKey.context!.findRenderObject() as RenderBox;
+          // placeholder = Rect.fromPoints(
+          //   renderViewer.localToGlobal(Offset.zero),
+          //   renderViewer
+          //       .localToGlobal(renderViewer.size.bottomRight(Offset.zero)),
+          // );
+          placeholder = Rect.fromCenter(
+            center: renderViewer
+                .localToGlobal(renderViewer.size.center(Offset.zero)),
+            width: renderViewer.size.width,
+            height: renderViewer.size.height,
           );
         });
-  }
+
+        entry = OverlayEntry(
+          builder: (context) => Obx(
+            () {
+              final _details = scaleDetails.value;
+              final panning = _details.pointerCount > 1;
+              final barrierOpacity = max(1.0, min(_details.scale, 1.70)) % 1;
+              final focal = panning ? _details.focalPoint : placeholder!.center;
+              final scale = max(
+                widget.minScale,
+                min(_details.scale, widget.maxScale),
+              );
+              return Container(
+                color: widget.barrierColor.withOpacity(barrierOpacity),
+                child: Stack(children: [
+                  AnimatedPositioned.fromRect(
+                    duration: (panning ? 50 : 150).milliseconds,
+                    curve: panning ? Curves.linear : Curves.easeOut,
+                    rect: Rect.fromCenter(
+                      center: focal,
+                      width: placeholder!.width * scale,
+                      height: placeholder!.height * scale,
+                    ),
+                    child: buildViewer(context),
+                  ),
+                ]),
+              );
+            },
+          ),
+        );
+        Overlay.of(context)?.insert(entry!);
+      },
+      onInteractionEnd: (details) {
+        scaleDetails.value = ScaleUpdateDetails(
+          focalPoint: placeholder?.center ?? scaleDetails.value.focalPoint,
+          horizontalScale: scaleDetails.value.horizontalScale,
+          localFocalPoint: scaleDetails.value.localFocalPoint,
+          pointerCount: scaleDetails.value.pointerCount,
+          rotation: scaleDetails.value.rotation,
+          scale: scaleDetails.value.scale,
+          verticalScale: scaleDetails.value.verticalScale,
+        );
+        snapTween = Matrix4Tween(
+          begin: controller.value,
+          end: Matrix4.identity(),
+        );
+        snap.value = 0;
+        snap.animateTo(
+          1,
+          duration: Duration(milliseconds: 250),
+          curve: Curves.ease,
+        );
+      });
 
   @override
   Widget build(BuildContext context) => Container(
-        child: placeholder != null
-            ? SizedBox.fromSize(size: placeholder!.size)
-            : buildViewer(context),
+        color: widget.backgroundColor ?? Get.theme.canvasColor,
+        width: placeholder?.width,
+        height: placeholder?.height,
+        child: placeholder == null ? buildViewer(context) : null,
       );
 }
